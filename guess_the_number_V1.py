4acbcfc65de68e962bb8ca8b02a4274a4e677d63
Vladislav <danilevicvladislav@gmail.com>
2024-07-05 17:46:09 +0300

    My second project called guess the number -V2

--- guess_the_number_V1.py
+++ guess_the_number_V1.py
@@ -1,16 +1,21 @@
 import random
-
+import sys
 
 def randomizer(Choiсe: int) -> int:
+    if Choiсe < 0:
+        raise ValueError("Пожалуйста, введите число корректно")
     result = random.randint(0, Choiсe)
     return result
 
-
-__user__choice = int(input('До какого числа вы выбираете случайность: '))
-result = randomizer(__user__choice)
-
+try:
+    __user__choice = int(input('До какого числа вы выбираете случайность: '))
+    result = randomizer(__user__choice)
+except ValueError:
+    print("Пожалуста, введите число корректно ")
+    sys.exit(1)
 
 def Guess_number():
+    try:
         while True:
             __user__quess = int(input('Угадайте число: '))
             if __user__quess == result:
@@ -20,6 +25,10 @@ def Guess_number():
                 print(f'Загаданное число больше')
             else:
                 print(f'Загаданное число меньше')
+    except ValueError:
+        print('Введите число корректно: ')
+
+
 
 
 print(Guess_number())
