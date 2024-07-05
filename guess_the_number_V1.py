12c6824b791174540588df3ca8eb34420f3d114b
Vladislav <danilevicvladislav@gmail.com>
2024-07-06 00:24:27 +0300

    Latest version of the project: added exception handler, eliminate the use of negative values

--- guess_the_number_V1.py
+++ guess_the_number_V1.py
@@ -1,34 +1,32 @@
 import random
-import sys
 
-def randomizer(Choiсe: int) -> int:
-    if Choiсe < 0:
-        raise ValueError("Пожалуйста, введите число корректно")
-    result = random.randint(0, Choiсe)
-    return result
 
-try:
-    __user__choice = int(input('До какого числа вы выбираете случайность: '))
-    result = randomizer(__user__choice)
-except ValueError:
-    print("Пожалуста, введите число корректно ")
-    sys.exit(1)
+def randomizer() -> int:
+    while True:
+        __user__choice = input('Enter the maximum number to choose a random number: ')
+        if __user__choice.isdigit() and int(__user__choice) > 0:
+            result = random.randint(0, int(__user__choice))
+            return result
+        else:
+            print('Please enter a valid number')
+
+
+result = randomizer()
 
 def Guess_number():
-    try:
-        while True:
-            __user__quess = int(input('Угадайте число: '))
-            if __user__quess == result:
-                print('Поздравляю вы угадали число')
-                return f'Загаданое число было: {__user__quess}'
+    while True:
+        __user__quess = input('Guess the number: ')
+        if __user__quess.isdigit():
+            __user__quess = int(__user__quess)
+            if __user__quess == result:  # Alternatively, you could use int() here instead of adding line 21
+                print('Congratulations, you guessed the number!')
+                return f'The guessed number was: {__user__quess}'
             elif __user__quess < result:
-                print(f'Загаданное число больше')
+                print('The guessed number is higher')
             else:
-                print(f'Загаданное число меньше')
-    except ValueError:
-        print('Введите число корректно: ')
-
-
+                print('The guessed number is lower')
+        else:
+            print('Please enter a valid number')
 
 
 print(Guess_number())
